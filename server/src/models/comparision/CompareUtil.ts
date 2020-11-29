78d648e6dc2c3b6de56f107c43356520ee291955
Hemanth Sai Nimmala <hemanthsai10@gmail.com>
2020-11-28 21:09:24 -0500

    Threshold selection for code matches and similarity percentage computation fix

--- server/src/models/comparision/CompareUtil.ts
+++ server/src/models/comparision/CompareUtil.ts
@@ -5,7 +5,14 @@ import ISyntaxTreeNode from '../content/ISyntaxTreeNode'
 import SubmissionMatch from './SubmissionMatch'
 import { MatchType } from '../schema/MatchType'
 import e = require('express')
+import { LINE_THRESHOLD_FOR_CODEMATCH, PERCENTAGE_THRESHOLD_FOR_CODEMATCH } from '../config/threshold'
 
+/**
+ * Utility to compute similarity for SubmissionMatch.
+ * Gives info about overall match between two user submissions.
+ *
+ * @param submissionMatch SubmissionMatch holding matches between two user submissions
+ */
 export function computeSimilarityPercentageBetweenSubmissions(submissionMatch: SubmissionMatch) {
   let overallCommonLines: number = 0
   let overallFileLines: number = 0
@@ -13,13 +20,23 @@ export function computeSimilarityPercentageBetweenSubmissions(submissionMatch: S
     overallCommonLines += fileMatch.getCommonLines()
     let file1 = fileMatch.getFile1().getSyntaxTree()
     let file2 = fileMatch.getFile2().getSyntaxTree()
-    let linesInFile1 = file1.getEndLineNumber() - file1.getStartLineNumber()
-    let linesInFile2 = file2.getEndLineNumber() - file2.getStartLineNumber()
+    let linesInFile1 = file1.getEndLineNumber() - file1.getStartLineNumber() + 1
+    let linesInFile2 = file2.getEndLineNumber() - file2.getStartLineNumber() + 1
     overallFileLines += Math.min(linesInFile1, linesInFile2)
   })
+  if (overallCommonLines === 0) {
+    return 0
+  }
   return Math.ceil((overallCommonLines / overallFileLines) * 100)
 }
 
+/**
+ * Utility to identify similarities between files of two users
+ *
+ * @param submissionMatch Submission Match for the user
+ * @param file1 File belonging user1
+ * @param file2 File belonging user2
+ */
 export function findSimilarities(submissionMatch: SubmissionMatch, file1: FileModel, file2: FileModel): FileMatch {
   let fileMatch = new FileMatch(file1, file2)
   let syntaxTree1: ISyntaxTreeNode = file1.getSyntaxTree()
@@ -32,6 +49,11 @@ export function findSimilarities(submissionMatch: SubmissionMatch, file1: FileMo
   return fileMatch
 }
 
+/**
+ * To compute similarity for file match
+ *
+ * @param fileMatch FileMatch
+ */
 function setSimilarlyPercentageForFile(fileMatch: FileMatch): void {
   let commonLines: number = 0
   fileMatch.getCodeMatches().map((codeMatch) => {
@@ -42,19 +64,42 @@ function setSimilarlyPercentageForFile(fileMatch: FileMatch): void {
       commonLines += matches.rangeOfNode1[1] - matches.rangeOfNode1[0]
     }
   })
+  fileMatch.setCommonLines(commonLines)
+  if (commonLines === 0) {
+    //If nothing in common between files
+    fileMatch.setSimilarityPercentage(0)
+    return
+  }
   let file1 = fileMatch.getFile1().getSyntaxTree()
   let file2 = fileMatch.getFile2().getSyntaxTree()
-  let linesInFile1 = file1.getEndLineNumber() - file1.getStartLineNumber()
-  let linesInFile2 = file2.getEndLineNumber() - file2.getStartLineNumber()
-  fileMatch.setCommonLines(commonLines)
-  fileMatch.setSimilarityPercentage(computeSimilarlyPercentage(commonLines, linesInFile1, linesInFile2))
+  let linesInFile1 = file1.getEndLineNumber() - file1.getStartLineNumber() + 1
+  let linesInFile2 = file2.getEndLineNumber() - file2.getStartLineNumber() + 1
+  fileMatch.setSimilarityPercentage(computeSimilarityPercentage(commonLines, linesInFile1, linesInFile2))
 }
 
-function computeSimilarlyPercentage(commonLines: number, linesInNode1: number, linesInNode2: number): number {
+/**
+ * Generic function to compute similarity for given common number of lines and number of lines in each node/file.
+ *
+ * @param commonLines Number of common lines
+ * @param linesInNode1 Number of lines in node 1
+ * @param linesInNode2 Number of lines in node 2
+ */
+function computeSimilarityPercentage(commonLines: number, linesInNode1: number, linesInNode2: number): number {
+  if (commonLines === 0) {
+    //not even single match
+    return 0
+  }
   let minLines = Math.min(linesInNode1, linesInNode2)
   return Math.ceil((commonLines / minLines) * 100)
 }
 
+/**
+ * Helper to identify matches across two files or two nodes.
+ *
+ * @param fileMatch FileMatch to hold all the matches between file1(or node1 of file1) and file2(or node2 of file2)
+ * @param node1 node representing file1
+ * @param node2 node representing file2
+ */
 function checkMatch(fileMatch: FileMatch, node1: ISyntaxTreeNode, node2: ISyntaxTreeNode): [number, number] {
   if (node1.getHashCode() === node2.getHashCode()) {
     let linesInNode1: number = node1.getStartLineNumber() - node1.getEndLineNumber() + 1
@@ -91,15 +136,18 @@ function checkMatch(fileMatch: FileMatch, node1: ISyntaxTreeNode, node2: ISyntax
         }
       }
     }
-    if (matchedLines.length > 0) {
+    if (matchedLines.length > LINE_THRESHOLD_FOR_CODEMATCH) {
       let linesIn1 = innerNodes1.filter(
         (innerNode1) => innerNode1.getStartLineNumber() === innerNode1.getEndLineNumber()
       ).length
       let linesIn2 = innerNodes2.filter(
         (innerNode2) => innerNode2.getStartLineNumber() === innerNode2.getEndLineNumber()
       ).length
-      let similarltyPercentage = computeSimilarlyPercentage(matchedLines.length, linesIn1, linesIn2)
-      fileMatch.addCodeMatch(new CodeMatch(node1, node2, similarltyPercentage, matchedLines))
+      let similarityPercentage = computeSimilarityPercentage(matchedLines.length, linesIn1, linesIn2)
+      //If similarity is high enough as per our config then add to file match else ignore
+      if (similarityPercentage > PERCENTAGE_THRESHOLD_FOR_CODEMATCH) {
+        fileMatch.addCodeMatch(new CodeMatch(node1, node2, similarityPercentage, matchedLines))
+      }
     }
   } else {
     //not relevant to compare
--- server/src/models/content/ProjectModel.ts
+++ server/src/models/content/ProjectModel.ts
@@ -1,11 +1,8 @@
 import { computeSimilarityPercentageBetweenSubmissions, findSimilarities } from '../comparision/CompareUtil'
 import Director from '../core/Director'
-import FileMatch from '../comparision/FileMatch'
 import FileModel from './FileModel'
-import IUserModel from '../user/IUserModel'
 import SubmissionMatch from '../comparision/SubmissionMatch'
 import SubmissionModel from './SubmissionModel'
-import AbstractUserModel from '../user/AbstractUserModel'
 import DetectionBuilder from '../core/Builder'
 
 export default class ProjectModel {
@@ -50,25 +47,34 @@ export default class ProjectModel {
     })
   }
 
+  /**
+   * Starts detection process
+   */
   runDetection() {
     let emails = Array.from(this.submissions.keys())
+    //For all possible combination of users
     for (let ind1 = 0; ind1 < emails.length - 1; ind1++) {
       for (let ind2 = ind1 + 1; ind2 < emails.length; ind2++) {
         let email1 = emails[ind1]
         let email2 = emails[ind2]
+        //Getting submission of users
         let sub1: SubmissionModel = this.submissions.get(email1)
         let sub2: SubmissionModel = this.submissions.get(email2)
+        //for all matches across the submissions of two users
         let submissionMatch = new SubmissionMatch(sub1, sub2)
         let files1: FileModel[] = sub1.getFiles()
         let files2: FileModel[] = sub2.getFiles()
+        //for all files belonging to users
         files1.forEach((file1) => {
           files2.forEach((file2) => {
             findSimilarities(submissionMatch, file1, file2)
           })
         })
         let similarityPercentage = computeSimilarityPercentageBetweenSubmissions(submissionMatch)
-        submissionMatch.setSimilarityPercentage(similarityPercentage)
-        this.submissionMatches.push(submissionMatch)
+        if (similarityPercentage > 0) {
+          submissionMatch.setSimilarityPercentage(similarityPercentage)
+          this.submissionMatches.push(submissionMatch)
+        }
       }
     }
   }
@@ -82,10 +88,6 @@ export default class ProjectModel {
         similarity: subMatch.getSimilarityPercentage(),
       }
     })
-  }
-
-  getSubmissionID(): number {
-    return 0
   }
 
   getSimilarity(similarityId: number): SubmissionMatch {
